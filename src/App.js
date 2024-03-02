1fb653f434b1cf12b44b1d4ba6af27a829965eb3
ahmed <sinkazseddiki852@gmail.com>
2024-03-02 22:40:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: 'Software Engineer',
+        imgSrc: 'https://example.com/image.jpg',
+        profession: 'Software Engineer'
+      },
+      shows: false,
+      lastMounted: null
+    };
+    this.toggleProfile = this.toggleProfile.bind(this);
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({ lastMounted: new Date() });
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(() => {
+      this.setState({ lastMounted: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  toggleProfile() {
+    this.setState(prevState => ({ shows: !prevState.shows }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {this.state.shows && (
+          <div>
+            <h2>{this.state.person.fullName}</h2>
+            <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+            <p>{this.state.person.bio}</p>
+            <p>{this.state.person.profession}</p>
+          </div>
+        )}
+        <p>Last mounted: {this.state.lastMounted ? this.state.lastMounted.toLocaleTimeString() : 'Never'}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
